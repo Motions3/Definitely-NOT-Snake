fca850088f29a978785d18e459718ee3f764d193
Antonio Maldonado III <motionscubed@gmail.com>
2024-07-08 01:40:51 +0100

    Update: Add Highscore

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
@@ -8,6 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("high_score.txt", "r") as high_score_file:
+            self.high_score = int(high_score_file.read())
         self.color("black")
         self.penup()
         self.goto(0, 260)
@@ -15,13 +18,17 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("high_score.txt", "w") as high_score_file:
+                high_score_file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
---
+++ test.py
@@ -0,0 +1,10 @@
+a = 1
+b = 2
+c = 3
+d = 1
+
+a = b
+b = c
+c = d
+
+print(a,b,c)
